0740cff0086595ca7e7f9cf0da3279e5f7b77a70
Natasa Ilic <natasa.ilic@sixsentix.com>
2022-03-24 08:56:49 +0100

    return largest numbers in arrays

--- ffc-basic-algorithm/05.js
+++ ffc-basic-algorithm/05.js
@@ -1,4 +1,6 @@
 /*
+https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/basic-algorithm-scripting/return-largest-numbers-in-arrays
+
 Return Largest Numbers in Arrays
 Return an array consisting of the largest number from each provided sub-array. For simplicity, the provided array will contain exactly 4 sub-arrays.
 

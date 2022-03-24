36db447655c5c42343f437a24115a849656fd24d
Natasa Ilic <natasa.ilic@sixsentix.com>
2022-03-24 08:57:42 +0100

    repeat a string

--- ffc-basic-algorithm/07.js
+++ ffc-basic-algorithm/07.js
@@ -1,4 +1,6 @@
 /*
+https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/basic-algorithm-scripting/repeat-a-string-repeat-a-string
+
 Repeat a String Repeat a String
 Repeat a given string str (first argument) for num times (second argument). 
 Return an empty string if num is not a positive number. For the purpose of this challenge, do not use the built-in .repeat() method.

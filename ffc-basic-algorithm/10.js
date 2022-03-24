1580ae5e490fa80ba8be6ac58b5df781257e0f97
Natasa Ilic <natasa.ilic@sixsentix.com>
2022-03-24 08:58:54 +0100

    boo whoo

--- ffc-basic-algorithm/10.js
+++ ffc-basic-algorithm/10.js
@@ -1,4 +1,6 @@
 /*
+https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/basic-algorithm-scripting/boo-who
+
 Boo who
 Check if a value is classified as a boolean primitive. Return true or false.
 
